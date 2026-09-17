3ebb9c6767132931635bcf69a263c0b5b9d018cf
kirti kaushik(kittu) <kirtikaushik1600@gmail.com>
2026-09-17 10:47:25 +0530

    added app.js file

---
+++ app.js
@@ -0,0 +1,9 @@
+const style = document.createElement('style');
+style.textContent = `
+  .container {
+    width: 100%;
+    min-height: 100vh;
+    background: linear-gradient(135deg, #ff6ec4, #7873f5);
+  }
+`;
+document.head.appendChild(style)
